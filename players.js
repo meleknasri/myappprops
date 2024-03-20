53e8dde744305ac9e1c188a654c7b6fa1536c128
meleknasri <150367418+meleknasri@users.noreply.github.com>
2024-03-20 14:59:12 +0100

    Add files via upload

---
+++ players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter-Miami club",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      imageURL: "https://example.com/messi.jpg"
+    },
+    
+    {
+        name: "Kevin DeBruyne",
+        team: "Manchester city",
+        nationality: "belgium",
+        jerseyNumber: 17,
+        age: 32,
+        imageURL: "https://example.com/KDB.jpg"
+      },
+
+      {
+        name: "Pablo Gavi",
+        team: "Barcelona",
+        nationality: "Spain",
+        jerseyNumber: 6,
+        age: 19,
+        imageURL: "https://example.com/gavi.jpg"
+      },
+
+      {
+        name: "Yan Sasse",
+        team: "Esperance Sportive Tunis",
+        nationality: "Brasil",
+        jerseyNumber: 10,
+        age: 26,
+        imageURL: "https://example.com/yansasse.jpg"
+      },
+    ];  
