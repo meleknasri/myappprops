6dd10d411296242076fefaecbd48e575f1966ac2
meleknasri <150367418+meleknasri@users.noreply.github.com>
2024-03-20 14:58:26 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PlayersList from "./PlayersList";
+
+function App() {
+  return (
+    <div>
+      <h1>Football Players</h1>
+      <PlayersList />
+    </div>
+  );
+}
+export default App;
